36e352bf217bee753b1865f0b7f1f73f10649e96
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-08-24 01:38:37 -0300

    First Footer Container created

--- Challenge_A/src/sections/Footer.jsx
+++ Challenge_A/src/sections/Footer.jsx
@@ -1,17 +1,54 @@
 import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import { logo } from "../images";
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center">
+    <footer className="flex w-full justify-start items-center">
       {/* First container */}
-      <div>
+      <div className="w-11/12 grid grid-cols-2 justify-items-start border-2 shadow-lg shadow-gray-700 shadow-">
         {/* Logo, span, icons*/}
-        <div>
-          <Facebook />
-          <Twitter />
-          <Instagram />
-          <Linkedin />
+        <div className="text-right mt-4">
+          <img
+            src={logo}
+            alt="Logo"
+            className="h-auto w-20 mx-auto rounded-xl"
+          />
+          <p className="text-gray-500 w-60 text-center mx-auto">
+            "Discover the Peace of Mind You Deserve with Insurezella Your
+            Reliable Insurance Umbrella"
+          </p>
+          <div className="flex space-x-4 mt-4 ml-16">
+            <Facebook />
+            <Twitter />
+            <Instagram />
+            <Linkedin />
+          </div>
         </div>
-        <div>{/* H2, column with <img> icon </img> + <span> */}</div>
+        <div className="flex-col justify-self-end mt-4">
+          {/* H2, column with <img> icon </img> + <span> */}
+          <h2 className="text-blue-900 font-semibold text-2xl">Services</h2>
+          <div className="flex-col space-y-1 mt-4 mr-10">
+            <div className="flex space-x-2">
+              <img src="" alt="icon" />
+              <span>Health Insurance</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="" alt="icon" />
+              <span>Life Insurance</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="" alt="icon" />
+              <span>Medicare Supplement</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="" alt="icon" />
+              <span>Medicare Advantage</span>
+            </div>
+            <div className="flex space-x-2">
+              <img src="" alt="icon" />
+              <span>(ACA)Obamacare</span>
+            </div>
+          </div>
+        </div>
       </div>
       {/* Second container */}
       <div>
