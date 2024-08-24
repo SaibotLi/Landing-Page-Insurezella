bbfd0c13652b884492b10e71f87940bf042b58a7
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-08-24 01:06:37 -0300

    Structured Mobile Footer

--- Challenge_A/src/sections/Footer.jsx
+++ Challenge_A/src/sections/Footer.jsx
@@ -1,5 +1,25 @@
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 const Footer = () => {
-  return <footer className="flex justify-center items-center">Footer</footer>;
+  return (
+    <footer className="flex justify-center items-center">
+      {/* First container */}
+      <div>
+        {/* Logo, span, icons*/}
+        <div>
+          <Facebook />
+          <Twitter />
+          <Instagram />
+          <Linkedin />
+        </div>
+        <div>{/* H2, column with <img> icon </img> + <span> */}</div>
+      </div>
+      {/* Second container */}
+      <div>
+        <div>{/* H2, column with <span> */}</div>
+        <div>{/* H2, column with <img> icon + <span> */}</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
